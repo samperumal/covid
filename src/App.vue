4a7663e370e48643237eeaf2c5ee21c6630a7939
Sam Perumal <sam_lki@yahoo.com>
2020-05-23 21:09:04 +0200

    Add help text to co-morbidities

--- src/App.vue
+++ src/App.vue
@@ -188,6 +188,7 @@
           <div class="title has-text-centered">Co-morbidities</div>
           <b-field 
             label="Alzheimer's or related dementia"
+            :message="alzheimerHelpText"
           >
             <b-field>
               <b-radio-button v-model="alzheimer"
@@ -210,7 +211,8 @@
             </b-field>
           </b-field>
           <b-field 
-            label="New York Heart Association heart failure"
+            label="Heart failure"
+            :message="heartHelpText"
           >
             <b-field>
               <b-radio-button v-model="heart"
@@ -233,7 +235,7 @@
             </b-field>
           </b-field>
           <b-field 
-            label="End-stage renal disease"
+            label="End-stage renal disease" :message="renalHelpText"
           >
             <b-field>
               <b-radio-button v-model="renalDisease"
@@ -256,21 +258,21 @@
             </b-field>
           </b-field>
           <b-field 
-            label="Cancer"
+            label="Cancer" :message="cancerHelpText"
           >
-            <b-field grouped group-multiline>
+            <b-field>
               <b-radio-button v-model="cancer"
                 native-value="0" expanded type="is-info"
               >
                 <span>N/A</span>
               </b-radio-button>
-              
+
               <b-radio-button v-model="cancer"
                 native-value="2" expanded type="is-info" 
               >
-                <span>Malignancy &lt; 10 year survival </span>
+                <span>&lt;10 year survival</span>
               </b-radio-button>
-              
+
               <b-radio-button v-model="cancer"
                 native-value="4" expanded type="is-info" 
               >
@@ -279,9 +281,9 @@
             </b-field>
           </b-field>
           <b-field 
-            label="Chronic lung disease (e.g. COPD, IPF)"
+            label="Chronic lung disease" :message="lungHelpText"
           >
-            <b-field grouped group-multiline>
+            <b-field>
               <b-radio-button v-model="lung"
                 native-value="0" expanded type="is-info"
               >
@@ -291,20 +293,20 @@
               <b-radio-button v-model="lung"
                 native-value="2" expanded type="is-info"
               >
-                <span>Moderately severe</span>
+                <span>Moderate</span>
               </b-radio-button>
               
               <b-radio-button v-model="lung"
                 native-value="4" expanded type="is-info"
               >
-                <span>Severe, evidence of frailty</span>
+                <span>Severe, frailty</span>
               </b-radio-button>
             </b-field>
           </b-field>
           <b-field 
-            label="Liver cirrhosis"
+            label="Liver cirrhosis" :message="cirrhosisHelpText"
           >
-            <b-field grouped group-multiline>
+            <b-field>
               <b-radio-button v-model="cirrhosis"
                 native-value="0" expanded type="is-info"
               >
@@ -314,30 +316,32 @@
               <b-radio-button v-model="cirrhosis"
                 native-value="2" expanded type="is-info"
               >
-                <span>Decompensation history</span>
+                <span>Decompensation</span>
               </b-radio-button>
               
               <b-radio-button v-model="cirrhosis"
                 native-value="4" expanded type="is-info"
               >
-                <span>Meld >= 20, no transplant</span>
+                <span>MELD ≥ 20</span>
               </b-radio-button>
             </b-field>
           </b-field>
-          <b-field label="Other conditions">
-            <b-field grouped group-multiline>
+          <b-field label="Other conditions"></b-field>
+          <b-field>
               <b-checkbox-button v-model="otherConditions"
                 native-value="CAD" expanded type="is-info"
               >
                 <span>Severe multi-vessel CAD</span>
               </b-checkbox-button>
-
+            </b-field>
+            <b-field
+              :message="aidsHelpText"
+            >
               <b-checkbox-button v-model="otherConditions"
                 native-value="AIDS" expanded type="is-info"
               >
                 <span>AIDS defining illness</span>
               </b-checkbox-button>
-            </b-field>
           </b-field>
           <div class="has-text-centered priority-score">Co-Morbidity Score: {{ morbidityScore }}</div>
         </section>
@@ -482,6 +486,58 @@ export default Vue.extend({
       if (this.renalCreat != null || this.renalUrine != null)
         return Math.max(this.renalCreat, this.renalUrine)
       else return null
+    },
+    alzheimerHelpText() {
+      if (this.alzheimer == "2")
+        return "Moderate Alzheimer's disease or related dementia."
+        
+      if (this.alzheimer == "4")
+        return "Severe Alzheimer's disease or related dementia."
+      return null
+    },
+    renalHelpText() {
+      if (this.renalDisease == "2")
+        return "End-stage renal disease in patients younger than 75 years."
+        
+      if (this.renalDisease == "4")
+        return "End-stage renal disease in patients older than 75 years."
+      return null
+    },
+    lungHelpText() {
+      if (this.lung == "2")
+        return "Moderately severe chronic lung disease (e.g. COPD, IPF)."
+        
+      if (this.lung == "4")
+        return "Severe chronic lung disease plus evidence of frailty."
+      return null
+    },
+    cirrhosisHelpText() {
+      if (this.cirrhosis == "2")
+        return "Cirrhosis with history of decompensation."
+        
+      if (this.cirrhosis == "4")
+        return "Cirrhosis with MELD score ≥ 20, ineligible for transplant."
+      return null
+    },
+    heartHelpText() {
+      if (this.heart == "2")
+        return "New York Heart Association Class III heart failure."
+        
+      if (this.heart == "4")
+        return "New York Heart Association Class IV heart failure, plus evidence of frailty."
+      return null
+    },
+    cancerHelpText() {
+      if (this.cancer == "2")
+        return "Malignancy with a < 10 year expected survival."
+      if (this.cancer == "4")
+        return "Cancer being treated with only palliative interventions (incl. chemotherapy and radiation)."
+      return null
+    },
+    aidsHelpText() {
+      if (this.otherConditions.includes("AIDS"))
+        return "AIDS defining illness, or viral load > 10,000 despite Rx."
+      return null
     }
   }
 });
